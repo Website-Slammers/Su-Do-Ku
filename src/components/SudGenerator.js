3895a89e90b77d2174515b9f40644b9d83d54b73
user_sunshine <ian.n.sorensen@gmail.com>
2023-02-23 11:31:22 -0700

    added random numbers, not spliced properly yet

--- src/components/SudGenerator.js
+++ src/components/SudGenerator.js
@@ -1,31 +1,29 @@
 // import React from 'react'
 
-// export const SudGenerator=()=>{
-    
-// }
+const sudGenerator=()=>{
+    let gridArray = []
+    let tempArray = [1,2,3,4,5,6,7,8,9];
+    for(let i =0; i<9; i++){
+        console.log("hello")
+        let passArray = []
 
-let newGrid = []
-let gridArray = []
-let tempArray = [1,2,3,4,5,6,7,8,9];
-for(let i =0; i<9; i++){
-    console.log("hello")
-    let passArray = []
-
-    for(j =0; j<9; j++){
-        let location =Math.floor(Math.random()*tempArray.length)
-        addedNumber = tempArray[location]
-        console.log('tempArray ', tempArray, tempArray.length)
-        console.log("added ", addedNumber)
-        passArray.push(addedNumber)
-        tempArray.splice(location,1)
-        // console.log(passArray)
+        for(let j =0; j<9; j++){
+            let location =Math.floor(Math.random()*tempArray.length)
+            let addedNumber = tempArray[location]
+            console.log('tempArray ', tempArray, tempArray.length)
+            console.log("added ", addedNumber)
+            passArray.push(addedNumber)
+            tempArray.splice(location,1)
+            // console.log(passArray)
+        }
+        gridArray.push(passArray)
+        passArray = []
+        tempArray = [1,2,3,4,5,6,7,8,9];
     }
-    gridArray.push(passArray)
-    passArray = []
-    tempArray = [1,2,3,4,5,6,7,8,9];
-
     console.log(gridArray)
-
+    return gridArray
+   
 }
 
-    
+sudGenerator();
+export default sudGenerator;
