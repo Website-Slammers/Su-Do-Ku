b24d3191ad4208f1a59116a4201d6744d59a5ceb
Andrew Cook <andrewjcook93@gmail.com>
2023-02-24 01:18:53 -0600

    respond play page with controls

--- src/components/Play.js
+++ src/components/Play.js
@@ -13,19 +13,36 @@ function Play() {
     <div id="play-return">
 
       <Header />
-      <Navbar />
+      {/* <Navbar /> */}
 
-      <h2 className="heading-secondary">Play Sudoku Now</h2>
+      {/* <h2 className="heading-secondary">Play Sudoku Now</h2> */}
 
-      <div className="flex-box flex-box--play">
+      <br /><br />
+
+      <div className="play">
 
         <div className="flex-box">
           <Puzzle />
         </div>
 
+      <div className="play__controls">
         <div className="flex-box">
-          <MobileControls />
-        </div>
+            <MobileControls />
+          </div>
+          <div className="play__number-box">
+            <button className="play__number play__number--1">1</button>
+            <button className="play__number play__number--2">2</button>
+            <button className="play__number play__number--3">3</button>
+            <button className="play__number play__number--4">4</button>
+            <button className="play__number play__number--5">5</button>
+            <button className="play__number play__number--6">6</button>
+            <button className="play__number play__number--7">7</button>
+            <button className="play__number play__number--8">8</button>
+            <button className="play__number play__number--9">9</button>
+            <button className="play__number play__number--X">X</button>
+          </div>
+      </div>
+
 
       </div>
 
