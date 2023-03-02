76d661b85141c8d35955905efff41d29c68ef574
Andrew Cook <andrewjcook93@gmail.com>
2023-03-02 16:44:33 -0600

    more responsive

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Outlet } from 'react-router-dom'
 
 import Navbar from "./Navbar";
+import Footer from './Footer'
 
 import './css/style.css'
 
@@ -10,6 +11,8 @@ const App = () =>{
         <div>
 
             <Outlet />
+
+            <Footer />
             
         </div>
     )
--- src/components/Play.js
+++ src/components/Play.js
@@ -49,7 +49,7 @@ function Play() {
 
       </div> {/* Play END */}
 
-      <Footer />
+
 
     </div>
   )
